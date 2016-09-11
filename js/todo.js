06ded4750591a9791d724a792352def1c598f490
gatjo108 <juraj.gajdos@webelevate.ie>
2016-09-11 14:30:49 +0100

    initial commit

---
+++ js/todo.js
@@ -0,0 +1,24 @@
+angular.module('todoApp', []).controller('TodoCtrl', function($scope) {
+    // List of todos	
+    $scope.todos = [
+        { text: 'Install This App', done: false },
+        { text: 'Add Something in This List', done: false }
+    ];
+
+    $scope.getTotalTodos = function() {
+        return $scope.todos.length;
+    };
+
+    // function to add item to todo list from the form input
+    $scope.addTodo = function() {
+        $scope.todos.push({ text: $scope.formTodoText, done: false });
+        $scope.formTodoText = ''; // clear input field
+    };
+
+    // function to clear completed items from todo list
+    $scope.clearCompleted = function() {
+        $scope.todos = $scope.todos.filter( function (todo) {
+        	return !todo.done;
+        	});
+    };
+});
